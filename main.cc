ac0e0c71293d99a6a7ea6a0173a75dc0ac327347
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2023-03-20 18:21:44 -0400

    done

--- main.cc
+++ main.cc
@@ -10,19 +10,14 @@ using ostp::severcc::server::ServerMode;
 using namespace ostp::severcc::server;
 
 std::function<void(const Request)> handler = [](const Request req) {
-    const std::string response =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "Content-Length: 11\r\n"
-        "\r\n"
-        "Hello World";
-    send(req.client_fd, response.c_str(), response.size(), 0);
-    close(req.client_fd);
+    std::cout << "Received request: " << req.data << std::endl;
 };
 
 int main() {
-    TcpServer tcp_server = TcpServer(8080);
-    Server &server = tcp_server;
+    // TcpServer tcp_server = TcpServer(8080);
+    // Server &server = tcp_server;
+    UdpServer udp_server = UdpServer(8080, ServerMode::SYNC);
+    Server &server = udp_server;
     server.register_default_processor(handler);
     server.run();
     return 0;
