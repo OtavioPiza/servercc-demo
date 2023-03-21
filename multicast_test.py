e1da8deb5c6e8f4facfff032281606742abc4498
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2023-03-21 13:49:40 -0400

    update demo

--- multicast_test.py
+++ multicast_test.py
@@ -6,5 +6,8 @@ ttl = 2
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+# s.setsockopt(socket.SOL_SOCKET, 25, b'lo')
+# s.sendto(b"Hello", (group, port))
+
 s.setsockopt(socket.SOL_SOCKET, 25, b'ztyxaydhop')
 s.sendto(b"Hello", (group, port))
