c3a2b4d8d3f4d599c6ccd56e9d6ab9cc7893ef1d
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2023-04-10 21:42:32 -0400

    d

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include "servercc.h"
 
 using ostp::servercc::distributed::DistributedServer;
+using namespace std;
 
 /// Echoes the tcp request.
 
@@ -26,14 +27,16 @@ int main(int argc, char *argv[]) {
     /// Port for multicast server UDP and for the TCP server.
     const int port = 8000;
 
+    set<string> peers;
+
     /// Create the distributed server.
     DistributedServer server(
         interface, interface_ip, group, port,
         [](const Request request) {
             std::cout << "Received request: " << request.data << std::endl;
         },
-        [](const std::string ip) { std::cout << "Peer connected: " << ip << std::endl; },
-        [](const std::string ip) { std::cout << "Peer disconnected: " << ip << std::endl; });
+        [&](const std::string ip) { peers.insert(ip); },
+        [&](const std::string ip) { peers.erase(ip); });
 
     server.add_handler("get", [&](const Request request) {
         server.log(Status::OK, "Received get request: " + request.data);
@@ -52,14 +55,67 @@ int main(int argc, char *argv[]) {
         // Write response to request.fd.
         write(request.fd, http_response.c_str(), http_response.size());
 
+        // For every peer, send a message with an echo request.
+        for (const auto &peer : peers) {
+            server.log(Status::OK, "Sending echo request to peer: " + peer);
+            server.send_message(peer, "echo hi");
+        }
+
         // Close the connection.
         close(request.fd);
     });
 
+    server.add_handler("echo", [&](const Request request) {
+        server.log(Status::OK, "Received echo request: " + request.data);
+
+        // Return without closing the connection since the connection will be
+        // reused.
+    });
+
     server.run();
 
+    sleep(2);
+
+    /// Start the shell.
+    cout << "\n\n"
+            "░██████╗███████╗██████╗░██╗░░░██╗███████╗██████╗░░█████╗░░█████╗░\n"
+            "██╔════╝██╔════╝██╔══██╗██║░░░██║██╔════╝██╔══██╗██╔══██╗██╔══██╗\n"
+            "╚█████╗░█████╗░░██████╔╝╚██╗░██╔╝█████╗░░██████╔╝██║░░╚═╝██║░░╚═╝\n"
+            "░╚═══██╗██╔══╝░░██╔══██╗░╚████╔╝░██╔══╝░░██╔══██╗██║░░██╗██║░░██╗\n"
+            "██████╔╝███████╗██║░░██║░░╚██╔╝░░███████╗██║░░██║╚█████╔╝╚█████╔╝\n"
+            "╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░╚════╝░░╚════╝░\n"
+            "\n\n";
+
     /// Sleep forever.
-    while (true) {
-        sleep(1);
-    }
+    string line = "";
+    do {
+        if (line == "peers") {
+            cout << "== Peers Currently Connected ==" << endl;
+
+            for (const auto &peer : peers) {
+                cout << "- \t" << peer << endl;
+            }
+            if (peers.empty()) {
+                cout << "No peers connected." << endl;
+            }
+
+            cout << "================================" << endl;
+        } else if (line == "exit") {
+            break;
+        } else if (line == "clear") {
+            system("clear");
+        } else if (line.starts_with("echo ")) {
+            string message = line.substr(5);
+            for (const auto &peer : peers) {
+                server.log(Status::OK, "Sending echo request to peer: " + peer);
+                server.send_message(peer, "echo " + message);
+            }
+        } else if (line.starts_with("mcast ")) {
+            string message = line.substr(6);
+            server.log(Status::OK, "Sending multicast request: " + message);
+            server.multicast_message(message);
+        }
+
+        cout << "sever-demo-shell: ";
+    } while (getline(cin, line));
 }
